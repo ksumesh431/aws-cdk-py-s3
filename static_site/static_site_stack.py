b704d8396cb8ae5cc70503954b8043c5bb41e118
ksumesh431 <sumesh42069@gmail.com>
2022-05-02 02:00:15 +0530

    initial push

--- static_site/static_site_stack.py
+++ static_site/static_site_stack.py
@@ -2,6 +2,9 @@ from aws_cdk import (
     # Duration,
     Stack,
     # aws_sqs as sqs,
+    aws_s3 as s3,
+    CfnOutput,
+    aws_cloudfront as cf
 )
 from constructs import Construct
 
@@ -9,11 +12,12 @@ class StaticSiteStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "StaticSiteQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        
+        bucket = s3.Bucket(self, "siteBucket",
+                           bucket_name="uniquenamesumesh2364746887",
+                           public_read_access=True,
+                           website_index_document="index.html")
+        
+        #Cloudformation Outputs
+        CfnOutput(self, "siteBucketName", value=bucket.bucket_name)
+        CfnOutput(self, "siteBucketWebsite", value=bucket.bucket_website_url)
